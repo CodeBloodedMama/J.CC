ad3cdb32fea50042e250d19e694991912d9b7f63
CodeBloodedMama <elisabethbinzer@hotmail.com>
2025-09-01 12:33:40 +0200

    Update footer text in App component to include developer credit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
-import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="app-shell">
       <header className="app-header">
         <div className="container">
-          <Link to="/" className="brand">JYSK Product Explorer</Link>
+          <Link to="/" className="brand">
+            JYSK Product Explorer - a codecase developed by CodeBloodedMama
+          </Link>
         </div>
       </header>
       <main className="container">
